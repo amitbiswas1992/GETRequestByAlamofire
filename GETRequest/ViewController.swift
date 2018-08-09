6e822358f27b666cbf6283f20529e3ede8d512a6
Amit Biswas <biswas1992@hotmail.com>
2018-08-09 13:23:52 -0400

    Implemented http request for bitcoin price

--- GETRequest/ViewController.swift
+++ GETRequest/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+   
+    static let kBPI = "bpi"
+    static let kUSD = "USD"
+    static let kRATE = "rate_float"
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        priceLabel.text = "..."
+        
+        Alamofire.request("https://api.coindesk.com/v1/bpi/currentprice.json").responseJSON{ response in
+            
+        print(response)
+            
+            if let bitcoinJSON = response.result.value {
+                
+                let bitcoinObject: Dictionary = bitcoinJSON as! Dictionary<String, Any>
+                
+                let bpiObject: Dictionary = bitcoinObject[ViewController.kBPI] as! Dictionary<String, Any>
+                
+                let usdObject : Dictionary = bpiObject[ViewController.kUSD] as! Dictionary<String, Any>
+                
+                let rate: Float = Float(truncating: usdObject[ViewController.kRATE] as! NSNumber)
+               
+            
+                
+                self.priceLabel.text = "$\(rate)"
+                
+            }
+
+        }
+    print("Loading Web services")
+    
+    
     }
 
+    
+    
 
+   
+    
 }
+
+
 
